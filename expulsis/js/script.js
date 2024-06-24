f76c2cf145c8c2b16882875760e866862a7ac927
Roman Budko <romanjaneiro@gmail.com>
2024-06-25 01:19:01 +0400

    fix: bug with blur and brightness of description

--- expulsis/js/script.js
+++ expulsis/js/script.js
@@ -4,12 +4,10 @@ const addDynamic = () => {
   const DESCRIPTION = document.querySelectorAll('.main__descr');
   const getElement = (className) => document.querySelector(className);
   
-  const [LEFTBUTTON, RIGHTBUTTON, LEFTSIDEIMAGE, RIGHTSIDEIMAGE] = 
+  const [LEFTBUTTON, RIGHTBUTTON] = 
   [
     getElement('.btn-left'), 
-    getElement('.btn-right'), 
-    getElement('.main__left-side'), 
-    getElement('.main__right-side')
+    getElement('.btn-right')
   ];
   
   const setListener = (button, elRender, className) => {
@@ -20,8 +18,6 @@ const addDynamic = () => {
   
   setListener(LEFTBUTTON, DESCRIPTION[0], 'main__descr_active');
   setListener(RIGHTBUTTON, DESCRIPTION[1], 'main__descr_active');
-  setListener(LEFTBUTTON, LEFTSIDEIMAGE, 'main_fade');
-  setListener(RIGHTBUTTON, RIGHTSIDEIMAGE, 'main_fade');
 }
 
 addDynamic()
